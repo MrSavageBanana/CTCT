086f5c04a8909dae48702b9fa7947e7a3299228f
MrSavageBanana <shaya5732@gmail.com>
2026-05-17 22:37:04 -0600

    Less redundant pacman hook script

--- vivaldimods.sh
+++ vivaldimods.sh
@@ -4,124 +4,23 @@
 FILE="/opt/vivaldi/resources/vivaldi/window.html"
 ANCHOR="<body>"
 
-INSERT1='<script src="video.js"></script>'
-INSERT2='<script src="shorts.js"></script>'
-INSERT3='<script src="youtube.js"></script>'
-INSERT4='<script src="search.js"></script>'
-INSERT5='<script src="posts.js"></script>'
-INSERT6='<script src="reddit_hp.js"></script>'
-INSERT7='<script src="reddit.js"></script>'
-INSERT8='<script src="startpage-wallpaper.js"></script>'
-INSERT9='<script src="bridge.js"></script>'
-INSERT10='<script src="autosave.js"></script>'
-INSERT11='<script src="loading.js"></script>'
-INSERT12='<script src="custom.js"></script>'
-INSERT13='<script src="dialogTab.js"></script>'
+INSERTS=('video.js' 'shorts.js' 'youtube.js' 'reddit_hp.js' 'reddit.js' 'startpage-wallpaper.js' 'bridge.js' 'autosave.js' 'loading.js' 'custom.js' 'dialogTab.js' 'tree.js' 'monochrome-icons.js' 'todoistDialog.js' 'youtubeNU.js' 'autocomplete-domain.js' 'toast.js' 'yandex.js' 'HibernatePanels.js' 'Markdown.js' 'Media.js')
+INSERT_END=("${INSERTS[@]/%/\"></script>}")
+INSERT_BEGIN=("${INSERT_END[@]/#/<script src=\"}")
+# DEBUG MESSAGES
+# echo "${INSERT_BEGIN[@]}"
+# echo 'EXECUTION COMMENCE!'
 # --- EXECUTION BLOCKS ---
-
-# INSERT1
-if grep -qF "$INSERT1" "$FILE"; then
-    echo "$INSERT1 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT1|" "$FILE"
-    echo "Inserted $INSERT1 with indentation."
-fi
-
-# INSERT2
-if grep -qF "$INSERT2" "$FILE"; then
-    echo "$INSERT2 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT2|" "$FILE"
-    echo "Inserted $INSERT2 with indentation."
-fi
-
-# INSERT3 
-if grep -qF "$INSERT3" "$FILE"; then
-    echo "$INSERT3 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT3|" "$FILE"
-    echo "Inserted $INSERT3 with indentation."
-fi
-
-# INSERT4
-if grep -qF "$INSERT4" "$FILE"; then
-    echo "$INSERT4 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT4|" "$FILE"
-    echo "Inserted $INSERT4 with indentation."
-fi
-
-# INSERT5
-if grep -qF "$INSERT5" "$FILE"; then
-    echo "$INSERT5 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT5|" "$FILE"
-    echo "Inserted $INSERT5 with indentation."
-fi
-
-# INSERT6
-if grep -qF "$INSERT6" "$FILE"; then
-    echo "$INSERT6 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT6|" "$FILE"
-    echo "Inserted $INSERT6 with indentation."
-fi
-
-# INSERT7
-if grep -qF "$INSERT7" "$FILE"; then
-    echo "$INSERT7 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT7|" "$FILE"
-    echo "Inserted $INSERT7 with indentation."
-fi
-
-# INSERT8
-if grep -qF "$INSERT8" "$FILE"; then
-    echo "$INSERT8 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT8|" "$FILE"
-    echo "Inserted $INSERT8 with indentation."
-fi
-
-# INSERT9
-if grep -qF "$INSERT9" "$FILE"; then
-    echo "$INSERT9 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT9|" "$FILE"
-    echo "Inserted $INSERT9 with indentation."
-fi
-
-# INSERT10
-if grep -qF "$INSERT10" "$FILE"; then
-    echo "$INSERT10 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT10|" "$FILE"
-    echo "Inserted $INSERT10 with indentation."
-fi
-
-# INSERT11
-if grep -qF "$INSERT11" "$FILE"; then
-    echo "$INSERT11 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT11|" "$FILE"
-    echo "Inserted $INSERT11 with indentation."
-fi
-
-# INSERT12
-if grep -qF "$INSERT12" "$FILE"; then
-    echo "$INSERT12 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT12|" "$FILE"
-    echo "Inserted $INSERT12 with indentation."
-fi
-# INSERT13
-if grep -qF "$INSERT13" "$FILE"; then
-    echo "$INSERT13 already exists. Skipping."
-else
-    sed -i "s|$ANCHOR|&\n\t$INSERT13|" "$FILE"
-    echo "Inserted $INSERT13 with indentation."
-fi
-
+for item in "${INSERT_BEGIN[@]}"; do
+	if grep -qF "$item" "$FILE"; then
+		DUPLICATE_MODS+=("$item")
+	else
+		sed -i "s|$ANCHOR|&\n\t$item|" "$FILE"
+		echo "Inserted $item with indentation."
+	fi
+done	
+ 
+echo "${#DUPLICATE_MODS[@]}" 'mods are already indented.'
 # Check that /etc/hosts isn't missing anything from the mirror, and add what's missing
 mirror=$(curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn-only/hosts \
   | sed 's/#.*//' \
@@ -143,9 +42,8 @@ if [ -z "$missing" ]; then
 else
   echo "Adding missing entries:"
   echo "$missing"
-  chattr -i
+  chattr -i /etc/hosts
   echo "$missing" | sudo tee -a /etc/hosts > /dev/null
-  chattr +ia
+  chattr +ia /etc/hosts
   echo "Done."
 fi
-
